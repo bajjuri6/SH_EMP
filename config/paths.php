9e1cf23b714c4c4ef4ee0d56211861e78c816fb2
venugopaltattari <venu.tattari@viveninfomedia.com>
2014-12-04 16:28:54 +0530

    Prepending http to URL value

--- config/paths.php
+++ config/paths.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('LIBRARY') || define('LIBRARY', dirname(__FILE__).'/libs');
-define('URL', $_SERVER['SERVER_NAME'].'/');
+define('URL', 'http://'.$_SERVER['SERVER_NAME'].'/');
 define('HR_MANAGER', 'hr_manager');
 define('PROJECT_MANAGER', 'Project_manager');
 define('HR', 'hr@saddahaq.com');
