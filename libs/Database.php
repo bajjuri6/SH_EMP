5b2ecf7229459366d75858e4db81852837d10bf0
Pallav Bajjuri <bajjuri@viveninfomedia.com>
2015-01-08 13:38:28 +0530

    Adding condition to change Database based on Server Name

--- libs/Database.php
+++ libs/Database.php
@@ -4,12 +4,10 @@ class Database extends PDO{
 
     function __construct() {
        if($_SERVER['SERVER_NAME'] == 'et.saddahaq.com'){
-         $this -> db = new PDO('mysql:host=localhost;dbname=SH_EMP', 'root', 'vivenfarms');
-      
+          parent::__construct('mysql:host=localhost;dbname=SH_EMP', 'root', 'vivenfarms');
        }
        else{
-         $this -> db = new PDO('mysql:host=localhost;dbname=SH_EMP_LIVE', 'root', 'vivenfarms');
-      
+          parent::__construct('mysql:host=localhost;dbname=SH_EMP_LIVE', 'root', 'vivenfarms'); 
       } 
     }
 
