54650815fff54257925e13dc6a11774d0ddbc0ba
Satish Kumar Mukkoju <mukkojusatish@gmail.com>
2015-01-10 02:59:50 +0530

    Constant changed in  Database.php file

--- libs/Database.php
+++ libs/Database.php
@@ -3,10 +3,10 @@
 class Database extends PDO{
 
     function __construct() {
-        if($_SERVER['SERVER_NAME'] == 'https://et.saddahaq.com'){
+        if($_SERVER['SERVER_NAME'] == 'et.saddahaq.com'){
                 parent::__construct('mysql:host=localhost;dbname=SH_EMP', 'root', 'vivenfarms');
         }else{
-                parent::__construct('mysql:host=localhost;dbname=SH_EMP_LIVE', 'root', 'vivenfarms');
+                parent::__construct('mysql:host=localhost;dbname=SH_EMP_LIVE', 'root', 'dambo');
         }
         
     }
