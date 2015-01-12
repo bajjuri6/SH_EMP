6c7e1ead8d47b2088c2a9748b4e8c79f25d8afcb
Satish Kumar Mukkoju <mukkojusatish@gmail.com>
2015-01-12 11:44:38 +0530

    database config changed

--- libs/Database.php
+++ libs/Database.php
@@ -5,8 +5,13 @@ class Database extends PDO{
     function __construct() {
         if($_SERVER['SERVER_NAME'] == 'et.saddahaq.com'){
                 parent::__construct('mysql:host=localhost;dbname=SH_EMP', 'root', 'vivenfarms');
-        }else{
-                parent::__construct('mysql:host=localhost;dbname=SH_EMP_LIVE', 'root', 'dambo');
+        }
+        elseif ($_SERVER['SERVER_NAME'] == 'http://i.viveninfomedia.com/')
+            {
+                parent::__construct('mysql:host=localhost;dbname=SH_EMP_LIVE', 'root', 'vivenfarms');
+        }
+        else{
+                parent::__construct('mysql:host=localhost;dbname=emp_mvc', 'root', 'dambo');
         }
         
     }
